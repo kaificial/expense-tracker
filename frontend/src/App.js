0ade0cae6e6722281c8680e4d1faec3f9d809fd0
Kevin Valencia <kevinvalencia@Kevins-MacBook-Pro.local>
2025-01-07 23:09:22 -0500

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import bg from './img/bg.png'
+import { MainLayout } from "./styles/Layouts";
+import Orb from "./components/Orb/Orb";
+import Navigation from "./components/Navigation/Navigation";
+
+function App() {
+
+  return (
+    <AppStyled bg={bg}className="App">
+      <Orb/>
+      <MainLayout>
+        <Navigation/>
+        </MainLayout>
+    </AppStyled>
+  );
+}
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props => props.bg});
+  position: relative;
+  `;
+
+
+export default App;
